dbaef028ad8c4847ae7acc0eb0193534770e560b
Ava Jeffs <avajeffs@Avas-MacBook-Pro-3.local>
2023-02-18 17:50:58 -0800

    Add test_add.sh file

---
+++ calculator/tests/test_add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+result=$(./calculator 2 + 1)
+if [ "$result" != "3" ]; then
+    echo "Test failed: addition of 1 and 2 should equal 3"
+    exit 1
+fi
+
+echo "Test passed: addition of 1 and 2 equals 3"
